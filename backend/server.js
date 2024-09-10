0aadc204e8253ddc6410f1a3c43698604f2b40d5
Garvit Sharma <sharmagarvit221999@gmail.com>
2024-09-11 00:37:50 +0530

    Model Js Change

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./models/user.model.js";
 dotenv.config();
 const app = express();
 
@@ -9,4 +10,5 @@ app.use("/api/v1/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+  connectDB();
 });
